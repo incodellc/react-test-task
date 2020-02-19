c31485bc7515a7c4d48ff59219587ce506eff944
Артём <33321841+tema1411@users.noreply.github.com>
2020-02-19 23:41:07 +0200

    add style for select

--- app/components/SelectInterval/SelectInterval.js
+++ app/components/SelectInterval/SelectInterval.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import './SelectInterval.scss';
 import {changeInterval} from '../../services';
 
 const SelectInterval = () => (
-        <select name="" id="" onChange={(e) => changeInterval(e.target.value)}>
+    <div className="select-interval">
+        <label htmlFor="select-interva" className="select-interval__label">Select update interval:</label>
+        <select name=""
+                id="select-interval"
+                onChange={(e) => changeInterval(e.target.value)}
+                className="select-interval__select"
+        >
             <option value="2000">2000</option>
             <option value="3000">3000</option>
             <option value="4000">4000</option>
-            <option value="5000">5000</option>
+            <option value="5000" selected>5000</option>
             <option value="6000">6000</option>
             <option value="7000">7000</option>
             <option value="8000">8000</option>
@@ -14,6 +21,8 @@ const SelectInterval = () => (
             <option value="10000">10000</option>
             <option value="11000">11000</option>
         </select>
-    );
+    </div>
+
+);
 
 export default SelectInterval;
