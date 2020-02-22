9d896acac267b720391e4ac3127efa5512375a08
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2020-02-22 21:37:39 +0200

    add tests for <TableHead>

---
+++ app/components/DataTable/TableHead.spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+
+import TableHead from './TableHead';
+
+describe('Tests from TableHead component', ()=>{
+	const wrapper = shallow(<TableHead />);
+	const thead = wrapper.find('thead');
+	it('The <thead> is drawn (only once)', ()=>{
+		expect(thead).to.have.length(1);
+	});
+	it('Only two <tr> are drawn', ()=>{
+	    const trArr = thead.shallow().find('tr');
+	    expect(trArr).to.have.length(2); 
+	});
+});
