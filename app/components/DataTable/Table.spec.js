c647a0188fb8acc291da9a0503a74d64347b5350
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2020-02-22 22:37:19 +0200

    added tests for Table component

--- app/components/DataTable/Table.spec.js
+++ app/components/DataTable/Table.spec.js
@@ -2,8 +2,15 @@ import React from 'react';
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
 
-import Tabel from './Tabel';
+import Table from './Table';
 
-discribe('Tests for Table component', ()=>{
-
+describe('Tests for Table component', ()=>{
+	const arr = [1,2,3,4,5,6,7];
+	const wrapper = shallow(<Table tickerDataArr={arr}/>);
+	it('Table tag rendering', ()=>{
+		expect(wrapper.find('table')).to.have.length(1);
+	});
+	it('Tbody tag rendering', ()=>{
+		expect(wrapper.find('tbody')).to.have.length(1);
+	});
 });
