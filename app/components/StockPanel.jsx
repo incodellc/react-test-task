7af69b35695c7a4def1b22019bd6638647c586d5
DanilGolenok <golenok.danil456@gmail.com>
2020-02-28 11:00:25 +0200

    Stock panel dynamic data

--- app/components/StockPanel.jsx
+++ app/components/StockPanel.jsx
@@ -1,27 +1,52 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import moment from 'moment'
 
 const StockPanel = ({ firstPrice, ticker }) => {
-  // const priceFound = firstPrice && ticker.price
-  // const priceChanged = priceFound ? (ticker.price - firstPrice).toFixed(2) : 0
+  const priceFound = firstPrice && ticker.price
+
+  let priceUnitChanged = 0
+  let pricePercentChanged = 0
+  let lastTrade = moment
+    .utc()
+    .startOf('day')
+    .fromNow()
+
+  if (priceFound) {
+    priceUnitChanged = (ticker.price - firstPrice).toFixed(2)
+    pricePercentChanged = (
+      ((ticker.price - firstPrice) / firstPrice) *
+      100
+    ).toFixed(2)
+
+    lastTrade = moment
+      .utc(ticker.last_trade_time)
+      .startOf('day')
+      .fromNow()
+  }
 
   return (
     <div className="stock-panel">
-      <div className="stock-symbol">
-        <span>{ticker.stockSymbol}</span>
+      <div className="exchange-info">
+        <span className="stock-symbol">{ticker.stockSymbol}</span>
+        <span className="exchange">{ticker.exchange}</span>
       </div>
       <div className="trade-info">
         <div className="price-changes">
           <span className="current-price">{ticker.price}</span>
-          <span className="change-units" data-change="increased">
-            3
+          <span
+            className="change-units"
+            data-change={priceUnitChanged >= 0 ? 'increased' : 'decreased'}>
+            {priceUnitChanged}
           </span>
-          <span className="change-percent" data-change="increased">
-            13%
+          <span
+            className="change-percent"
+            data-change={pricePercentChanged >= 0 ? 'increased' : 'decreased'}>
+            {pricePercentChanged}%
           </span>
         </div>
         <div className="last-trade">
-          <span className="time">last trade: 5sec ago</span>
+          <span className="time">last trade: {lastTrade}</span>
         </div>
       </div>
     </div>
