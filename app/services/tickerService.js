bd4260f1c3e51aacd858b1b502ca75229d1ef93a
denis-fominykh <fominykhdenis@gmail.com>
2020-03-31 21:49:09 +0300

    dispatching the data from ticker service

--- app/services/tickerService.js
+++ app/services/tickerService.js
@@ -1,14 +1,16 @@
 import io from 'socket.io-client';
+import { updateData } from '../actions/actions';
 
 let socket = null;
 
-export const connect = (stockSymbol) => {
+export const connect = (store, stockSymbol) => {
   socket = io('http://localhost:4000');
 
   socket.on('connect', () => {
     console.log('connected');
 
     socket.on(stockSymbol, (data) => {
+      store.dispatch(updateData(data));
       console.log(data);
     });
 
