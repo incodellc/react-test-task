57879fbcd9657bc80acf991ba4e81fd483570dfc
oksanana <oksanana@wizardsdev.com>
2020-09-28 00:57:34 +0300

    test task

--- app/services/tickerService.js
+++ app/services/tickerService.js
@@ -2,13 +2,14 @@ import io from 'socket.io-client';
 
 let socket = null;
 
-export const connect = (stockSymbol) => {
+export const connect = (stockSymbol, handler) => {
     socket = io('http://localhost:4000');
 
     socket.on('connect', () => {
         console.log('connected');
 
         socket.on(stockSymbol, (data) => {
+            handler(data);
             console.log(data);
         });
 
